8c17b996d3138348433979358cd9a251b4badf0a
Raz Al-Jaf <razaljaf@gmail.com>
2020-12-01 23:05:50 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -59,7 +59,7 @@ def hashketball(game_hash)
     }
   },
     {:away=>
-    :team_name=>"Charlotte Hornets"
+    {:team_name=>"Charlotte Hornets",
     :colors=>["Turquoise, Purple"]
     :players=>
       [
