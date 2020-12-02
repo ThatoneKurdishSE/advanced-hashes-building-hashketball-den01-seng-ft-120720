0e01feef9e223de33c9be1d1ba1f57bf47b78a8e
Raz Al-Jaf <razaljaf@gmail.com>
2020-12-02 00:07:49 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -15,7 +15,7 @@ def game_hash
            },
           {:player_name=>"Reggie Evans",
             :number=>30,
-              :shoe=>12,
+              :shoe=>14,
                 :points=>12,
                   :rebounds=>12,
                     :assists=>12,
